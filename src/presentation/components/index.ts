f75ec3d6a06b5b6398c6d5ff49c3cc199ec7655c
Thaynã Ferreira <t.f.sousa0@gmail.com>
2022-11-27 16:14:24 -0300

    feat: technical challenge - Thaynã Ferreira

---
+++ src/presentation/components/index.ts
@@ -0,0 +1,4 @@
+export * from "./list/List";
+export * from "./to-do/Todo";
+export * from "./task-card/TaskCard";
+export * from "./arrow/Arrow";
---
+++ src/presentation/pages/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+import { Todo } from "../components";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div
+          style={{
+            marginLeft: 25,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+          }}
+        >
+          <h1>Welcome To The Every.io Code Challenge.</h1>
+          <div style={{ marginLeft: 50 }}>
+            <a
+              target="_blank"
+              href="https://www.figma.com/proto/kd49ArXbBt0vi1kBSLkmC1/Code-Challenge?node-id=1%3A2&scaling=min-zoom&page-id=0%3A1"
+              style={{
+                color: "white",
+                background: "black",
+                padding: 8,
+                borderRadius: 4,
+                display: "inline-block",
+              }}
+              rel="noreferrer"
+            >
+              Checkout the Prototype
+            </a>
+          </div>
+        </div>
+      </header>
+      <main
+        style={{
+          width: "calc(100vw - 50px)",
+          margin: "0 auto",
+          maxHeight: "100%",
+          maxWidth: "1399px",
+          background: "#fcfcfc",
+          border: "1px solid #000000",
+          boxSizing: "border-box",
+          boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+          borderRadius: 40,
+        }}
+      >
+        <Todo />
+      </main>
+    </div>
+  );
+}
+
+export default App;
